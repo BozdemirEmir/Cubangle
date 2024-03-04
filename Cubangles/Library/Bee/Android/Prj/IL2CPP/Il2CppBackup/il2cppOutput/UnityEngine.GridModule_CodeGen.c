c9e2a178f2568826356389b569d0ac330fefce05
Eyics <nozoemir@gmail.com>
2024-03-04 21:35:01 +0300

    File

---
+++ Cubangles/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/UnityEngine.GridModule_CodeGen.c
@@ -0,0 +1,56 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void GridLayout_CellToLocalInterpolated_mE1FC35F36111BD0881573C6F51C37239BF3BD621 (void);
+extern void GridLayout_WorldToCell_m72AFBAA2458CD70A302708AA09DC2641BD95E21E (void);
+extern void GridLayout_DoNothing_mA280987BF98D257023D46C2C01902FC82EE6A00A (void);
+extern void GridLayout_CellToLocalInterpolated_Injected_m64A34FF84B0948E3E0CAD199855CBCE3434976D0 (void);
+extern void GridLayout_WorldToCell_Injected_mFC4155CB0C1B36A15550FCE30F22A799676F939E (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	GridLayout_CellToLocalInterpolated_mE1FC35F36111BD0881573C6F51C37239BF3BD621,
+	GridLayout_WorldToCell_m72AFBAA2458CD70A302708AA09DC2641BD95E21E,
+	GridLayout_DoNothing_mA280987BF98D257023D46C2C01902FC82EE6A00A,
+	GridLayout_CellToLocalInterpolated_Injected_m64A34FF84B0948E3E0CAD199855CBCE3434976D0,
+	GridLayout_WorldToCell_Injected_mFC4155CB0C1B36A15550FCE30F22A799676F939E,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	6509,
+	6512,
+	9044,
+	3195,
+	3195,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_UnityEngine_GridModule_CodeGenModule;
+const Il2CppCodeGenModule g_UnityEngine_GridModule_CodeGenModule = 
+{
+	"UnityEngine.GridModule.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
